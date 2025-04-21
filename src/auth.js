5f7f183991a074c765482f355c19cb10ba43885a
miilthon92 <55821491+miilthon92@users.noreply.github.com>
2025-04-21 00:34:09 +0000

    commit 9

--- src/auth.js
+++ src/auth.js
@@ -1,16 +1,21 @@
-// src/auth.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-// Simular login
-export function loginUser() {
-    localStorage.setItem('proRoomUser', 'activo');
-  }
-  
-  // Simular logout
-  export function logoutUser() {
-    localStorage.removeItem('proRoomUser');
-  }
-  
-  // Verificar si está logueado
-  export function isUserLoggedIn() {
-    return localStorage.getItem('proRoomUser') === 'activo';
-  }
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyC1DYIWz8oyyBphion3oQZa8MnJ4HR4YH0",
+  authDomain: "proroom-b5aea.firebaseapp.com",
+  projectId: "proroom-b5aea",
+  storageBucket: "proroom-b5aea.appspot.com",
+  messagingSenderId: "49808156718",
+  appId: "1:49808156718:web:5861bd367a86ff676997bf",
+  measurementId: "G-6ZG66370Y3"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializar autenticación
+const auth = getAuth(app);
+
+export { auth };
